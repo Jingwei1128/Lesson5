755199350d9819e323519e75492dc4bb603f084f
Jingwei1128 <42337842+Jingwei1128@users.noreply.github.com>
2022-12-23 17:03:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,7 @@ from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
-tmpData = '''{
-    "title":"loc",
-    "data":[{
-      "name":"abc-1234",
-      "loc":[
-        {"lng":121.536099,"lat":25.028136},
-        {"lng":121.534426,"lat":25.075181},
-        {"lng":121.558623,"lat":25.038996}
-      ]
-    },
-    {
-      "name":"abc-4567",
-      "loc":[
-        {"lng":121.546099,"lat":25.028136},
-        {"lng":121.644426,"lat":25.075181},
-        {"lng":121.748623,"lat":25.038996}
-      ]
-    }]
-}'''
+tmpData = {"title":"loc","data":[{"name":"abc-1234","loc":[{"lng":121.536099,"lat":25.028136},{"lng":121.534426,"lat":25.075181},{"lng":121.558623,"lat":25.038996}]},{"name":"abc-4567","loc":[{"lng":121.546099,"lat":25.028136},{"lng":121.644426,"lat":25.075181},{"lng":121.748623,"lat":25.038996}]}]}
 
 @app.route("/")
 def home():
